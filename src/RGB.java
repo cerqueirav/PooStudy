d98cf274c9ffe51a67fe4c340eed295c8feb65fc
cerqueirav <victorcerx@gmail.com>
2021-08-29 00:27:37 -0300

    subindo novos arquivos

--- src/RGB.java
+++ src/RGB.java
@@ -1,7 +1,23 @@
 public class RGB{
     
-    
-    public RGB (){
+    private int red;
+    private int green;
+    private int blue;
+
+    public RGB(int r, int g, int b){
 
     }
+
+    public int getRed(){
+        return this.red;
+    }
+
+    public int getGreen(){
+        return this.green;
+    }
+
+    public int getBlue(){
+        return this.blue;
+    }
+
 }
